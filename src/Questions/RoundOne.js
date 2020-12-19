d468a2d05cb6bb88e98bf575ce7516cb409dfb7a
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-12-18 19:52:55 -0700

    Add round 1 data

---
+++ src/Questions/RoundOne.js
@@ -0,0 +1,106 @@
+const roundOneQuestions =
+    [
+        {
+            category: "Entertainment: Television",
+            questions: [
+                {
+                    question: "In the TV series &quot;Person of Interest&quot;, who plays the character &quot;Harold Finch&quot;?",
+                    answer: "Michael Emerson"
+                },
+                {
+                    question: "Who co-founded the YouTube Let&#039;s Play channel &quot;Game Grumps&quot; alongside Newgrounds animator Egoraptor?",
+                    answer: "JonTron"
+                },
+                {
+                    question: "In &quot;The Simpsons&quot;, what is the real name of &quot;Comic Book Guy&quot;?",
+                    answer: "Jeff Albertson"
+                },
+                {
+                    question: "Baron Silas Greenback is the arch nemesis of which cartoon hero?",
+                    answer: "Danger Mouse"
+                },
+                {
+                    question: "In the Sci-Fi television show Doctor Who, who plays the Tenth Doctor?",
+                    answer: "David Tennant"
+                }
+            ]
+        },
+        {
+            category: "Science: Computers",
+            questions: [
+                {
+                    question: "The computer OEM manufacturer Clevo, known for its Sager notebook line, is based in which country?",
+                    answer: "Taiwan"
+                },
+                {
+                    question: "Which coding language was the #1 programming language in terms of usage on GitHub in 2015?",
+                    answer: "JavaScript",
+                },
+                {
+                    question: "Which of the following is a personal computer made by the Japanese company Fujitsu?",
+                    answer: "FM-7",
+                },
+                {
+                    question: "What is known as &quot;the brain&quot; of the Computer?",
+                    answer: "Central Processing Unit",
+                },
+                {
+                    question: "Approximately how many Apple I personal computers were created?",
+                    answer: "200",
+                }
+            ]
+        },
+        {
+            category: "Geography",
+            questions: [
+                {
+                    question: "Which state of the United States is the smallest?",
+                    answer: "Rhode Island ",
+                },
+                {
+                    question: "What are the four corner states of the US?",
+                    answer: "Utah, Colorado, Arizona, New Mexico",
+                },
+                {
+                    question: "What is the busiest port in Europe?",
+                    answer: "Port of Rotterdam",
+                },
+                {
+                    question: "Which of the following is the longest river in Europe?",
+                    answer: "Volga",
+                },
+                {
+                    question: "Which English county will you find the University of East Anglia?",
+                    answer: "Norfolk",
+                }
+            ]
+        },
+        {
+            category: "History",
+            questions: [
+                {
+                    question: "During the Mongolian invasions of Japan, what were the Mongol boats mostly stopped by?",
+                    correct_answer: "Typhoons",
+                },
+                {
+                    question: "How many times was Albert Einstein married in his lifetime?",
+                    answer: "Twice",
+                },
+                {
+                    question: "Who was the leader of the Communist Party of Yugoslavia ?",
+                    answer: "Josip Broz Tito",
+                },
+                {
+                    question: "When was the Grand Patriotic War in the USSR concluded?",
+                    answer: "May 9th, 1945",
+                },
+                {
+                    question: "What happened on June 6, 1944?",
+                    answer: "D-Day",
+                }
+
+            ]
+        }
+    ]
+
+export default roundOneQuestions;
