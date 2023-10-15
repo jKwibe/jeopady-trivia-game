0bc22649543eac5c445f35df58d6e9d4f7167a46
Kwibe Merci <46441816+jKwibe@users.noreply.github.com>
2023-10-15 11:44:50 -0600

    Update Footer.js

--- frontend/src/Components/Footer/Footer.js
+++ frontend/src/Components/Footer/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
                 <p>This app was built in tribute to Alex Trebek by Kwibe Merci</p>
             </div>
             <div className="main-footer_social">
-                <a className="social-icon" href="https://github.com/jKwibe" target="_blank" rel="noreferrer"><i className="fab fa-github"></i></a>
+                <a className="social-icon" href="https://github.com/kwibem" target="_blank" rel="noreferrer"><i className="fab fa-github"></i></a>
                 <a className="social-icon" href="https://www.linkedin.com/in/kwibe-merci/" target="_blank" rel="noreferrer"><i className="fab fa-linkedin-in"></i></a>
             </div>
         </footer>
